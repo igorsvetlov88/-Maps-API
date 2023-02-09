0d8a9328debddfacab836312b7e087ce733e5fd7
qwer123423637423 <thesembn@yandex.ru>
2023-02-09 19:38:53 +0300

    design pyqt

--- main.py
+++ main.py
@@ -1,1 +1,41 @@
-# test commit
+import os
+import pygame
+import requests
+import sys
+
+class MapParams(object):
+    def __init__(self):
+        self.lat = 61.665279
+        self.lon = 50.813492
+        self.zoom = 16
+        self.type = "map"
+
+    def ll(self):
+        return str(self.lon) + "," + str(self.lat)
+
+
+def load_map(mp):
+    map_request = "http://static-maps.yandex.ru/1.x/?ll={ll}&z={z}&l={type}".format(ll=mp.ll(), z=mp.zoom, type=mp.type)
+    response = requests.get(map_request)
+    if not response:
+        print("Ошибка выполнения запроса:")
+        print(map_request)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        sys.exit(1)
+
+    map_file = "map.png"
+    try:
+        with open(map_file, "wb") as file:
+            file.write(response.content)
+    except IOError as ex:
+        print("Ошибка записи временного файла:", ex)
+        sys.exit(2)
+    return map_file
+
+
+def main():
+    window = PyQt5.
+
+
+if __name__ == "__main__":
+    main()
