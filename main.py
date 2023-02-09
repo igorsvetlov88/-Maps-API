043607b49f4a052535f5a6289a5b95bfc663fc97
igor_svetlov88 <82970004+igorsvetlov88@users.noreply.github.com>
2023-02-09 20:25:30 +0300

    simplified mapparams

--- main.py
+++ main.py
@@ -6,24 +6,22 @@ from design import *
 
 class MapParams(object):
     def __init__(self):
-        self.lat = 61.665279
-        self.lon = 50.813492
-        self.zoom = 16
-        self.type = "map"
+        self.shirota = 0.665279
+        self.dolgota = 0.813492
+        self.z = 0
 
     def ret(self):
-        return str(self.lon) + "," + str(self.lat)
+        return str(self.shirota) + "," + str(self.dolgota)
 
 
 def load_map(mp):
-    map_request = "http://static-maps.yandex.ru/1.x/?ll={ll}&z={z}&l={type}".format(ll=mp.ret(), z=mp.zoom, type=mp.type)
+    map_request = f"http://static-maps.yandex.ru/1.x/?ll={mp.ret()}&z={mp.z}&l=map"
     response = requests.get(map_request)
-    if not response:
-        print("Ошибка выполнения запроса:")
-        print(map_request)
-        print("Http статус:", response.status_code, "(", response.reason, ")")
-        sys.exit(1)
-
+#     if not response:
+#         print("Ошибка выполнения запроса:")
+#         print(map_request)
+#         print("Http статус:", response.status_code, "(", response.reason, ")")
+#         sys.exit(1)
     map_file = "map.png"
     try:
         with open(map_file, "wb") as file:
@@ -33,9 +31,10 @@ def load_map(mp):
         sys.exit(2)
     return map_file
 
-
+TODO:
 def main():
     design
+    def draw_map(map_file)
 
 if __name__ == "__main__":
     main()
